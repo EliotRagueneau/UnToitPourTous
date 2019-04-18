c6d2d5cbbbf74774306733b927ae68fc81d12cb8
jeremybulle <jeremyjp.bulle@gmail.com>
2019-04-18 20:25:51 +0200

    ajout de la fonction Agency::reSell() dans le menu principal

--- pied.cpp
+++ pied.cpp
@@ -28,19 +28,20 @@ int main() {
         cout << "\t-1. Ajouter un bien\n";
         cout << "\t-2. Ajouter un vendeur\n";
         cout << "\t-3. Ajouter un acheteur\n";
+		cout << "\t-4. Remettre en vente un bien\n";
         cout << "\t-----------------------\n";
-        cout << "\t-4. Rechercher un bien (Pour le visiter, faire une proposition de vente, ou en accepter une)\n";
+        cout << "\t-5. Rechercher un bien (Pour le visiter, faire une proposition de vente, ou en accepter une)\n";
         cout << "\t-----------------------\n";
-        cout << "\t-5. Visiter un bien a partir de l'adresse du bien\n";
-        cout << "\t-6. Proposer une offre a partir de l'adresse du bien\n";
-        cout << "\t-7. Faire une vente a partir de l'adresse du bien\n";
+        cout << "\t-6. Visiter un bien a partir de l'adresse du bien\n";
+        cout << "\t-7. Proposer une offre a partir de l'adresse du bien\n";
+        cout << "\t-8. Faire une vente a partir de l'adresse du bien\n";
         cout << "\t-----------------------\n";
-        cout << "\t-8. Afficher les biens\n";
-        cout << "\t-9. Afficher les vendeurs\n";
-        cout << "\t-10. Afficher les acheteurs potentiels\n";
+        cout << "\t-9. Afficher les biens\n";
+        cout << "\t-10. Afficher les vendeurs\n";
+        cout << "\t-11. Afficher les acheteurs potentiels\n";
         cout << "\t-----------------------\n";
-        cout << "\t-11. Sauvegarder l'agence\n";
-        cout << "\t-12. Recharger l'agence\n";
+        cout << "\t-12. Sauvegarder l'agence\n";
+        cout << "\t-13. Recharger l'agence\n";
         cout << "\t------------------------\n";
         cout << "\t-0. Quitter le programme\n";
 
@@ -57,35 +58,38 @@ int main() {
             case 3: // Ajouter un acheteur
                 unToitPourTous.addBuyer();
                 break;
-            case 4: // Rechercher un bien
+			case 4: // Remettre en vente un bien
+				unToitPourTous.reSell();
+				break;
+            case 5: // Rechercher un bien
                 unToitPourTous.search();
                 break;
-            case 5: // Visiter via adresse
+            case 6: // Visiter via adresse
                 unToitPourTous.visit();
                 break;
-            case 6: // Proposition d'offre via adresse
+            case 7: // Proposition d'offre via adresse
                 unToitPourTous.addProposal();
                 break;
-            case 7: // Faire une vente via adresse
+            case 8: // Faire une vente via adresse
                 unToitPourTous.sell();
                 break;
 
 
 
-            case 8: // Afficher les biens
+            case 9: // Afficher les biens
                 unToitPourTous.show();
                 break;
-            case 9: // Afficher les vendeurs
+            case 10: // Afficher les vendeurs
                 unToitPourTous.showSellers();
                 break;
-            case 10: // Afficher les acheteurs potentiels
+            case 11: // Afficher les acheteurs potentiels
                 unToitPourTous.showBuyers();
                 break;
 
-            case 11: // Sauvegarder les biens
+            case 12: // Sauvegarder les biens
                 unToitPourTous.save();
                 break;
-            case 12: // Charger les biens à partir d'un fichier
+            case 13: // Charger les biens à partir d'un fichier
                 unToitPourTous.load();
                 break;
             case 0: // Quitter le programme
